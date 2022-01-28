1a4b854909ccfb976decfcfa99882157eca5fb89
Zhixian Ong <zhixian@hotmail.com>
2022-01-28 16:23:52 +0800

    kiv

---
+++ Blaze/Models/UserState.cs
@@ -0,0 +1,33 @@
+﻿using Microsoft.AspNetCore.Components.Authorization;
+using System.Security.Claims;
+
+namespace Blaze.Models;
+
+public class UserState
+{
+    public event Action? OnChange;
+
+    private AuthenticationState authenticationState = new AuthenticationState(new ClaimsPrincipal());
+
+    public AuthenticationState AuthenticationState
+    {
+        get => authenticationState;
+        set
+        {
+            authenticationState = value;
+            NotifyStateChanged();
+        }
+    }
+
+    public ClaimsPrincipal ClaimsPrincipal
+    {
+        get => authenticationState.User;
+        set
+        {
+            authenticationState = new AuthenticationState(value);
+            NotifyStateChanged();
+        }
+    }
+
+    private void NotifyStateChanged() => OnChange?.Invoke();
+}
--- Blaze/Program.cs
+++ Blaze/Program.cs
@@ -3,6 +3,7 @@ using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
 using Blaze;
 using Microsoft.AspNetCore.Components.Authorization;
 using Blaze.Services;
+using Blaze.Models;
 
 var builder = WebAssemblyHostBuilder.CreateDefault(args);
 
@@ -17,9 +18,12 @@ builder.Services.AddOptions();
 
 builder.Services.AddAuthorizationCore();
 
-builder.Services.AddScoped<AuthenticationStateProvider, CustomAuthStateProvider>();
+builder.Services.AddScoped<AuthenticationStateProvider, AuthenticatedUserStateProvider>();
 
 builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
 
+// Register states
+builder.Services.AddSingleton<AppStateContainer>();
+builder.Services.AddSingleton<UserState>();
 
 await builder.Build().RunAsync();
